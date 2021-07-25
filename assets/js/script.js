7f95778cbe130fe3d5e9d63e10d49f79f34aa6ba
Philip <williamsphilip@outlook.com>
2021-07-25 08:21:48 +0100

    Added js for moves, timer and reset

---
+++ assets/js/script.js
@@ -0,0 +1,113 @@
+const deckCards = ["RJ.png", "RG.png", "QH.png", "QH.png", "KD.png", "KD.png", "KC.png", "KC.png", "JS.png", "JS.png", "JR.png", "JR.png", "AH.png", "AH.png", "10C.png", "10C.png"];
+
+const deck = document.querySelector(".deck");
+let opened = [];
+let matched = [];
+const modal = document.getElementById("modal");
+const reset = document.querySelector(".reset-btn");
+const playAgain = document.querySelector(".play-again-btn");
+const movesCount = document.querySelector(".moves-counter");
+let moves = 0;
+const star = document.getElementById("star-rating").querySelectorAll(".star");
+let starCount = 3;
+const timeCounter = document.querySelector(".timer");
+let time;
+let minutes = 0;
+let seconds = 0;
+let timeStart = false;
+
+// Shuffle Deck
+function shuffle(array) {
+    let currentIndex = array.length, temporaryValue, randomIndex;
+  
+    while (currentIndex !== 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+    return array;
+}
+
+// Start Game
+function startGame() {
+    // Invoke shuffle function and store in variable
+    const shuffledDeck = shuffle(deckCards); 
+    // Iterate over deck of cards array
+    for (let i = 0; i < shuffledDeck.length; i++) {
+      // Create the <li> tags
+      const liTag = document.createElement('LI');
+      // Give <li> class of card
+      liTag.classList.add('card');
+      // Create the <img> tags
+      const addImage = document.createElement("IMG");
+      // Append <img> to <li>
+      liTag.appendChild(addImage);
+      // Set the img src path with the shuffled deck
+      addImage.setAttribute("src", "img/" + shuffledDeck[i]);
+      // Add an alt tag to the image
+      addImage.setAttribute("alt", "image of vault boy from fallout");
+      // Update the new <li> to the deck <ul>
+      deck.appendChild(liTag);
+    }
+  }
+  
+  startGame();
+
+  function removeCard() {
+    // As long as <ul> deck has a child node, remove it
+    while (deck.hasChildNodes()) {
+      deck.removeChild(deck.firstChild);
+    }
+  }
+
+// Timer 
+function timer() {
+    // Update the count every 1 second
+    time = setInterval(function() {
+      seconds++;
+        if (seconds === 60) {
+          minutes++;
+          seconds = 0;
+        }
+      // Update the timer in HTML with the time it takes the user to play the game
+      timeCounter.innerHTML = "<i class='fa fa-hourglass-start'></i>" + " Timer: " + minutes + " Mins " + seconds + " Secs" ;
+    }, 1000);
+  }
+
+  function stopTime() {
+    clearInterval(time);
+  }
+
+// Reset
+function resetEverything() {
+    // Stop time, reset the minutes and seconds update the time inner HTML
+    stopTime();
+    timeStart = false;
+    seconds = 0;
+    minutes = 0;
+    timeCounter.innerHTML = "<i class='fa fa-hourglass-start'></i>" + " Timer: 00:00";
+    // Reset star count and the add the class back to show stars again
+    star[1].firstElementChild.classList.add("fa-star");
+    star[2].firstElementChild.classList.add("fa-star");
+    starCount = 3;
+    // Reset moves count and reset its inner HTML
+    moves = 0;
+    movesCount.innerHTML = 0;
+    // Clear both arrays that hold the opened and matched cards
+    matched = [];
+    opened = [];
+    // Clear the deck
+    removeCard();
+    // Create a new deck
+    startGame();
+  }
+
+// Moves 
+function movesCounter() {
+    // Update the html for the moves counter
+    movesCount.innerHTML ++;
+    // Keep track of the number of moves for every pair checked
+    moves ++;
+  }
